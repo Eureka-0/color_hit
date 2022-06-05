805078d55e90a9efa747f9c40f1f45bd73c5fede
Eureka <56858288+sunflower6@users.noreply.github.com>
2022-06-05 14:50:29 +0800

    split collide detection from GameView into external function.

--- views.py
+++ views.py
@@ -1,36 +1,49 @@
 from functools import reduce
-from random import randint, shuffle, sample
+from random import randint, sample, shuffle
+from typing import Union
 
 import pygame as pg
+from pygame.event import Event
+from pygame.sprite import Group, Sprite
 
 from config import *
-from widgets import Disc, Pie, Pin, Bullets
+from widgets import Bullets, Disc, Pie, Pin
 
 
 def collide_mask(sprite1, sprite2):
-    return False if pg.sprite.collide_mask(sprite1, sprite2) is None else True
+    return True if pg.sprite.collide_mask(sprite1, sprite2) else False
 
 
-def expand_colors(colors: list[str], num: list[int]):
-    expand = reduce(lambda x, y: x + y, [[color] * n for n, color in zip(num, colors)])
+def hit_correct_color(pin: Pin, disc: Disc) -> Union[None, bool]:
+    collision = pg.sprite.spritecollideany(pin, disc, collide_mask)
+    if isinstance(collision, Pie):
+        if pin.color == collision.color:
+            return True
+        else:
+            return False
+    elif isinstance(collision, Pin):
+        return False
+
+
+def expand_colors(colors: list[str], num: list[int]) -> list[str]:
+    expand = reduce(list.__add__, [[color] * n for color, n in zip(colors, num)])
     shuffle(expand)
     return expand
 
 
 def rand_colors(num: int) -> list[str]:
     if 1 <= num <= 4:
-        colors = [RED, GREEN, ORANGE, PURPLE]
-        shuffle(colors)
-        return colors[0:num]
+        shuffle(COLORS)
+        return COLORS[0:num]
     else:
         return ["#000000"]
 
 
-def rand_num(n):
+def rand_num(n: int) -> list[int]:
     sam = [0] + sample(range(1, PIN_NUM), n - 1) + [PIN_NUM]
     sam.sort()
     num = [sam[i + 1] - sam[i] for i in range(n)]
-    upper = round(PIN_NUM * 3 / (n + 2))
+    upper = round(PIN_NUM / n) + 2
     while max(num) > upper:
         sam = [0] + sample(range(1, PIN_NUM), n - 1) + [PIN_NUM]
         sam.sort()
@@ -67,12 +80,12 @@ class GameView:
         self.bullets = Bullets(self.screen, self.colors)
         self.colors.pop()
 
-    def on_keydown(self, event):
+    def on_keydown(self, event: Event):
         if event.key == pg.K_SPACE and self.pin.mode == STILL:
             self.pin.mode = SHOOT
             self.bullets.number -= 1
 
-    def on_mousedown(self, event):
+    def on_mousedown(self, event: Event):
         if event.button == pg.BUTTON_LEFT and self.pin.mode == STILL:
             self.pin.mode = SHOOT
             self.bullets.number -= 1
@@ -84,10 +97,10 @@ class GameView:
             self.level += 1
             self.init_level()
 
-    def get_widgets(self):
+    def get_widgets(self) -> list[Union[Sprite, Group]]:
         widgets = []
         for v in self.__dict__.values():
-            if isinstance(v, (pg.sprite.Sprite, pg.sprite.Group)):
+            if isinstance(v, (Sprite, Group)):
                 widgets.append(v)
         return widgets
 
@@ -95,16 +108,14 @@ class GameView:
         if self.pin.rect.top >= WINDOW_SIZE[1]:
             self.next_pin()
 
-        collision = pg.sprite.spritecollideany(self.pin, self.disc, collide_mask)
-        if isinstance(collision, Pie):
-            if self.pin.color == collision.color:
+        if self.pin.mode == SHOOT:
+            correct = hit_correct_color(self.pin, self.disc)
+            if correct:
                 self.pin.mode = PRICK
                 self.disc.add(self.pin)
                 self.next_pin()
-            else:
+            elif correct is False:
                 self.pin.mode = DROP
-        elif isinstance(collision, Pin):
-            self.pin.mode = DROP
 
         for widget in self.get_widgets():
             widget.update()
